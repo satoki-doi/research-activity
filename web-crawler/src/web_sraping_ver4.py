9eaa95c1f4b2b30e5ee388770e2f8c81aff57851
Satoki_doi <satoki_doi@ohnolab-no-iMac.local>
2016-07-25 16:16:43 +0900

    update [web_scraiping_ver4.py]

---
+++ web-crawler/src/web_sraping_ver4.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import time
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+import os.path
+import cPickle
+from numpy.random import *
+import ssl
+from functools import wraps
+
+dir = "../output_data"
+OPENER = urllib2.build_opener()
+OPENER.addheaders = [("User-Agent", "Mozilla/4.0")]
+# http://coreblog.org/ats/python-http-module-request/ 参照
+base = 'https://www.google.co.jp/search?site=imghp&tbm=isch&source=hp&num=30&q='
+
+
+def SEARCH_WORD_GetHTML(que):
+    """
+    Google検索にアクセスする
+    """
+    url = base + urllib.quote("\"%s\"" % que)
+    html = OPENER.open(url).read()
+    soup = BeautifulSoup(html, "lxml")
+    return soup
+
+
+def DL_img(img_url, word):
+    """
+    IMGファイルのダウンロード
+    """
+    try:
+        img = urllib2.urlopen(img_url)
+        fname = img_url.split("/")[-1]
+        file = '%s/%s/%s.jpg' % (dir, word, fname)
+        if os.path.isfile(file) is False:
+            localfile = open(file, "w")
+            print file
+            localfile.write(img.read())
+            img.close()
+            localfile.close()
+        else:
+            print "%s is already exist" % file
+    except:
+        print "Not Download %s" % img_url
+
+
+def Get_imgURL(soup, word):
+    Namelist = soup.find_all('img')
+    for link in Namelist:
+        try:
+            img_url = link.get('src')
+            DL_img(img_url, word)
+            # stop_time = poisson(lam=1) + 1
+            # time.sleep(stop_time)
+        except Exception as e:
+            print e
+
+# 商品名の読み取り
+
+
+def pickle_load(file_name):
+    """
+    file loading
+    """
+    fh = open(file_name)
+    result = cPickle.load(fh)
+    fh.close
+    return result
+
+
+def sslwrap(func):
+    @wraps(func)
+    def bar(*args, **kw):
+        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+        return func(*args, **kw)
+    return bar
+
+
+def main(item_list):
+    """
+    web scraiping start
+    """
+    count = 0
+    for item in item_list:
+        print print_text % item + '[%s]' % count
+        item_dir = '../output_data/%s' % item
+        if os.path.exists(item_dir) is False:
+            os.mkdir(item_dir)
+        else:
+            print "dir already exist"
+        if len(os.listdir(item_dir)) < 20:
+            soup = SEARCH_WORD_GetHTML(item)
+            Get_imgURL(soup, item)
+            stop_time = poisson(lam=3) + 1
+            time.sleep(stop_time)
+            count += 1
+        else:
+            print """
+            ------------------------------
+            ------------------------------
+            already collected img_file
+            ------------------------------
+            ------------------------------
+            """
+            count += 1
+
+if __name__ == '__main__':
+    file_name = "../input_data/item_list.pkl"
+    item_data = pickle_load(file_name)
+    item_list = item_data['item_name'].values()
+    print_text = """
+        -----------------------------------
+        -----------------------------------
+        scraipin %s .......
+        -----------------------------------
+        -----------------------------------
+        """
+    ssl.wrap_socket = sslwrap(ssl.wrap_socket)
+    main(item_list)
